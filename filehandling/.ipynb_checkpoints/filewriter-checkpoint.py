bab984e704d925db29dea53f0e41afebaf107375
alohomora2024 <alohomora2024@gmail.com>
2024-09-17 12:01:00 +0000

    .

--- filehandling/.ipynb_checkpoints/filewriter-checkpoint.py
+++ filehandling/.ipynb_checkpoints/filewriter-checkpoint.py
@@ -1,5 +1,5 @@
 name = "ACF"
-file = open("newfile.text", 'w')
+file = open("newfile1.text", 'w')
 file.write(f"Hello, {name}!\n")
 file.write("Isn’t this amazing?\n")
 file.write("that we can create and write on text files\n")

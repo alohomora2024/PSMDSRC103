d89e63e7d6fdca983a73b1b0eb2be3ec1db69398
alohomora2024 <alohomora2024@gmail.com>
2024-09-17 10:51:34 +0000

    .

--- filehandling/filewriter.py
+++ filehandling/filewriter.py
@@ -1,7 +1,7 @@
-name = “ACF”
-file = open(“newfile:text”, ‘w’)
-file.write(f”Hello, {name}!\n”)
-file.write(“Isn’t this amazing?\n”)
-file.write(“that we can create and write on text files\n”)
-file.write(“using Python.”)
+name = "ACF"
+file = open("newfile.text", 'w')
+file.write(f"Hello, {name}!\n")
+file.write("Isn’t this amazing?\n")
+file.write("that we can create and write on text files\n")
+file.write("using Python.")
 file.close()

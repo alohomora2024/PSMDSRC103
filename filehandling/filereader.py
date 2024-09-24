53518e67e1260d40e2552286eadb84e12261af82
alohomora2024 <alohomora2024@gmail.com>
2024-09-24 09:43:56 +0000

    .

---
+++ filehandling/filereader.py
@@ -0,0 +1,6 @@
+file_path = /workspaces/PSMDSRC103/newfile1.txt
+file = open(file_path, 'r')
+data= file.read()
+print(data)
+file.close()
+
---
+++ filehandling/filereader_updated.py
@@ -0,0 +1,5 @@
+file = open("newfile1", 'r')
+data= file.read()
+print(data)
+file.close()
+
